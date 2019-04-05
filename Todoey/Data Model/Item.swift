1f8aa541225760673a55e97b7cf06e3e8798d73b
Roman Savko <roman.savko@me.com>
2019-04-05 12:01:39 +0300

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Roman Savko on 4/5/19.
+//  Copyright © 2019 Roman Savko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false 
+}
